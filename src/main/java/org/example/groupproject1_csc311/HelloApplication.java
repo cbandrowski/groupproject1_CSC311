66c44d6db89265835d1ffadb5b383f7a0175ff3d
“Cody <cbandrowski@gmail.com//>
2024-09-19 10:41:15 -0400

    first edit

--- src/main/java/org/example/groupproject1_csc311/HelloApplication.java
+++ src/main/java/org/example/groupproject1_csc311/HelloApplication.java
@@ -10,14 +10,14 @@ import java.io.IOException;
 /**
  * Title: Group Project 1
  * Description:
- * @author Cody Bandrowski,
+ * @author Cody Bandrowski, Other Authors
  */
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Group Project 1 ");
+        stage.setTitle("Group Project 1  ");
         stage.setScene(scene);
         stage.show();
     }
